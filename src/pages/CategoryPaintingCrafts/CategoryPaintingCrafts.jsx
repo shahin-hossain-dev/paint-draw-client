225c56d8d5e6ad0de21b8e5e163777a5fef4aa1f
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-04-30 00:47:46 +0600

    category based data page functinalty updated

--- src/pages/CategoryPaintingCrafts/CategoryPaintingCrafts.jsx
+++ src/pages/CategoryPaintingCrafts/CategoryPaintingCrafts.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../shared/Navbar/Navbar";
 import { useLoaderData, useParams } from "react-router-dom";
 import CategoryCraftCard from "../../components/CategoryCraftCard/CategoryCraftCard";
 
 const CategoryPaintingCrafts = () => {
+  //   enter page to view from top
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   // specific category data loader
   const categoryCrafts = useLoaderData();
   console.log(categoryCrafts);
@@ -12,7 +16,7 @@ const CategoryPaintingCrafts = () => {
     <div>
       <Navbar />
 
-      <div className="w-[90%] md:w-[90%] lg:w-[85%] mx-auto ">
+      <div className="w-[90%] md:w-[90%] lg:w-[85%] mx-auto mb-12">
         <div className="grid grid-col-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12">
           {categoryCrafts.map((craft) => (
             <CategoryCraftCard
